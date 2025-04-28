e0f19ed96fa3e4e5694bc9d2227ef55df4eaf880
Krystian.kuznik1 <krystian15082003@gmail.com>
2025-04-28 22:14:44 +0200

    Poprawiono literowke

--- test/test_views.py
+++ test/test_views.py
@@ -15,4 +15,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"KrystI", "msg":"Hello World!"}', rv.data)
+        self.assertEqual(b'{ "imie":"Krysti", "msg":"Hello World!"}', rv.data)
