ec35f7ef5eb3709e051cd3b684a9b1f6058fe461
Krystian.kuznik1 <krystian15082003@gmail.com>
2025-04-28 22:07:20 +0200

    Poprawiono imie 2

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Krysti"
+moje_imie = "KrystI"
 msg = "Hello World!"
 
 
